705d6164c7d955b39c19f06545167cb1cdc7536f
Nasir <nasir-1996@hotmail.co.uk>
2013-08-08 10:32:28 +0100

    new file:   app/views/search.php

---
+++ app/views/search.php
@@ -0,0 +1,52 @@
+<?php include(app_path().'/includes/_header.php'); ?>
+
+</head>
+
+<?php include(app_path().'/includes/_titlebar.php') ?>
+
+    <div class="jumbotron">
+        <h1 style="text-align:center">Find A Project!</h1>
+        <div class="row">
+            <div class="col-lg-2">
+
+            </div><!-- /.col-lg-6 -->
+            <div class="col-lg-8">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Enter your Postcode! e.g. NW7 4HN">
+		      <span class="input-group-btn">
+		        <button class="btn" style="background-color: rgb(0, 150, 230);"type="button"><img src="/img/glyphicons_242_google_maps.png" /></button>
+		      </span>
+                </div><!-- /input-group -->
+            </div><!-- /.col-lg-6 -->
+        </div><!-- /.row -->
+    </div>
+
+    <div class="body-content">
+		<ol>
+			<li style="list-style-type: none; float:left;">
+			<a href="#link"><h5 style="padding:0; margin:0; font-size:16pt;">Title</h5></a>
+			<p style="padding:0; margin:0; width:65%; float:left;">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget nunc massa. Proin ultricies tellus elementum tellus suscipit, eget aliquam erat vehicula. Maecenas scelerisque risus nisl, ut eleifend diam egestas ut. Aenean vel elit mattis, laoreet mauris non, lacinia diam. Praesent quam libero, fringilla ut libero vitae, rhoncus venenatis lorem. Praesent vitae euismod sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+			</p>
+			<img src="http://placehold.it/350x200" style="float:right; margin:0 10px 0 10px;"/>
+			<div style="width:600px;">
+			<a href="#page"><span>Linked1</span></a>
+			<a href="#page"><span>Linked2</span></a>
+			<a href="#page"><span>Linked3</span></a>
+			</div>
+			</li>
+			<li style="list-style-type: none; float:left;">
+			<a href="#link"><h5 style="padding:0; margin:0; font-size:16pt;">Title</h5></a>
+			<p style="padding:0; margin:0; width:65%; float:left;">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget nunc massa. Proin ultricies tellus elementum tellus suscipit, eget aliquam erat vehicula. Maecenas scelerisque risus nisl, ut eleifend diam egestas ut. Aenean vel elit mattis, laoreet mauris non, lacinia diam. Praesent quam libero, fringilla ut libero vitae, rhoncus venenatis lorem. Praesent vitae euismod sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+			</p>
+			<img src="http://placehold.it/350x200" style="float:right; margin:0 10px 0 10px;"/>
+			<div style="width:600px;">
+			<a href="#page"><span>Linked1</span></a>
+			<a href="#page"><span>Linked2</span></a>
+			<a href="#page"><span>Linked3</span></a>
+			</div>
+			</li>
+		</ol>
+    </div><!-- /.body-content -->
+    <?php include(app_path().'/includes/_footer.php') ?>
