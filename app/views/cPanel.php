a62da6445a0a669eedb4ad741704e2402206c9f8
Nasir <nasir-1996@hotmail.co.uk>
2013-08-08 16:05:38 +0100

    modified:   app/views/cPanel.php 	modified:   app/views/home.php 	modified:   app/views/search.php

--- app/views/cPanel.php
+++ app/views/cPanel.php
@@ -10,13 +10,21 @@
                 <div class="col-lg-2">
 
                 </div><!-- /.col-lg-6 -->
-                <div class="col-lg-8">
+				<div class="col-lg-8">
+				<form method="post" action="establish.dev/search">
                     <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Enter your Postcode! e.g. NW7 4HN">
+                        <input id="query" type="text" class="form-control" placeholder="Enter your Postcode! e.g. NW7 4HN">
 		      <span class="input-group-btn">
 		        <button class="btn" style="background-color: rgb(0, 150, 230);"type="button"><img src="/img/glyphicons_242_google_maps.png" /></button>
 		      </span>
                     </div><!-- /input-group -->
+				  <div>
+					  <center>
+						  <input id="projects" type="radio" name="sex" value="male">Projects</input>
+						  <input id="properties" type="radio" name="sex" value="female">Properties</input>
+					  </center>
+				  </div>
+				</form>
                 </div><!-- /.col-lg-6 -->
             </div><!-- /.row -->
 
