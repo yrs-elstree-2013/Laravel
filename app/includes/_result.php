0544d65a21954c05317c34a09e6785146ae31296
Alexander Christie <Alexander.Christie@westminster.org.uk>
2013-08-08 10:34:14 +0100

    Adjustments

--- app/includes/_result.php
+++ app/includes/_result.php
@@ -1,4 +1,3 @@
-<ol>
 <li style="list-style-type: none; float:left;">
 <a href="#link"><h5 style="padding:0; margin:0; font-size:16pt;">Title</h5></a>
 <p style="padding:0; margin:0; width:65%; float:left;">
@@ -11,16 +10,3 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget nunc massa. 
 <a href="#page"><span>Linked3</span></a>
 </div>
 </li>
-<li style="list-style-type: none; float:left;">
-<a href="#link"><h5 style="padding:0; margin:0; font-size:16pt;">Title</h5></a>
-<p style="padding:0; margin:0; width:65%; float:left;">
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget nunc massa. Proin ultricies tellus elementum tellus suscipit, eget aliquam erat vehicula. Maecenas scelerisque risus nisl, ut eleifend diam egestas ut. Aenean vel elit mattis, laoreet mauris non, lacinia diam. Praesent quam libero, fringilla ut libero vitae, rhoncus venenatis lorem. Praesent vitae euismod sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-</p>
-<img src="http://placehold.it/350x200" style="float:right; margin:0 10px 0 10px;"/>
-<div style="width:600px;">
-<a href="#page"><span>Linked1</span></a>
-<a href="#page"><span>Linked2</span></a>
-<a href="#page"><span>Linked3</span></a>
-</div>
-</li>
-</ol>
