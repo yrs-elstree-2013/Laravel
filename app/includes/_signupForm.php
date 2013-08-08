cd08d9190c1a6bad8c36862801cc72f7e338b5d1
Nasir <nasir-1996@hotmail.co.uk>
2013-08-08 13:34:55 +0100

    new file:   app/includes/_signupForm.php 	new file:   app/views/signup.php

---
+++ app/includes/_signupForm.php
@@ -0,0 +1,16 @@
+<h3>Sign up</h3>
+<hr style="bacgkround-color:#E0E0E0; color:#E0E0E0; border-color:E0E0E0; margin-bottom:20px;"/>
+<div class="col-lg-6">
+<h4>Create your free personal account</h4>
+<form style="font-weight:bold;">
+Username: <input type="text" /> <br/>
+Email Address: <input type="text" /> <br/>
+Password: <input type="text" /> <br/>
+Confirm Password: <input type="text" />
+<p style="background-color:#E1E1E1; border:1px solid #E0E0E0;"> By clicking on "Signup" below, you are agreeing to the <a href="#terms">Terms of Services</a></p>
+<input type="submit" value="Signup"/>
+</form>
+</div>
+<div class="col-lg-6" style="font-size:18px;">
+To create projects and invest in properties, you are required to signup for a account.
+</div>
